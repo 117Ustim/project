e27d979bf200bf8fa01209b728e51135ace431ca
Ustim <ustik72@gmail.com>
2021-12-10 11:37:30 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+//alert('ytllo');
+// result = confirm('ты еще здесь?');
+//console.log(result);
+//const anser = prompt ('Вам есть 18?','');
+//console.log(anser);
+
+const ans = [];
+
+ans[0] = prompt('Как вас зовут?','');   
+ans[1] = prompt('Ваша фамилия?','');
+ans[2] = prompt('Сколько лет?','');
+
+document.write(ans);
+
