83d7a7122629700efb9fc7a004840f7e486118ad
Ustim <ustik72@gmail.com>
2022-02-17 19:33:29 +0200

    index.html mod

---
+++ app8.js
@@ -0,0 +1,286 @@
+// let a = [1,2,3,4,1,2,3,4];
+
+// for( let i = 0;i < a[1];i++) {
+// document.querySelector('.one').innerHTML += a[i];
+// }
+
+// let a = [3, 8, 'string', false];
+// let out = document.querySelector('.out');
+// document.querySelector('button').onclick = function () {
+// for (let i = 0; i < a.length; i++)
+// out.value += a[i] + ' ';
+// };
+
+// let a = [2, 4, 8, 12, 33];
+// let one = document.querySelector('.one');
+
+// for (let  i = 0; i < a.length;i++) {
+
+// }
+//  one.innerHTML += a.length ;  
+
+
+// let m = [3, 5, 'fare', 2, 9, 'go-go'];
+
+// let one = document.querySelector('.one');
+
+// one.innerHTML += m[2]+ m[3]+ m[5];
+
+
+// let m = [9, 23, 5, 33, 45, 21, 67];
+// let one = document.querySelector('.one');
+
+// let sum = m[0] + m[2] + m[3];
+// one.innerHTML += sum;
+
+
+
+// let m = ['Ustim','Taurus','14 may','1972'];
+// let one = document.querySelector('.one');
+
+// document.querySelector('button').onclick = function ()  {
+//     for( let i = 0;i < m.length;i++) {
+//       one.innerHTML += m[i] + ' ' ; 
+
+
+//     }
+//     one.innerHTML += m[0] + m[3];
+// }; 
+
+
+// let a =[];
+// a['ar7'] = [' china',' india',' brasil',' japan',' egipty'];
+// let one = document.querySelector('.one');
+
+// document.querySelector('button').onclick = function ()  {
+
+//     a.ar7.push(' italy',' turkey',' vietnam');
+//   one.innerHTML += a['ar7'] + ' ';  
+
+// };
+
+
+// let one = document.querySelector('.one');
+// m = ['test', 'west', 'list', 'class', 'best'];
+// document.querySelector('button').onclick = function () {
+
+//   let t = m[0];
+// let a = m.length -1;
+// let b =m[a];
+// m[0] = m[a];
+// m[a] = t;
+
+// one.innerHTML += m ;
+
+// };
+
+
+// let one = document.querySelector('.one');
+// let m = ['test', 'west', 'list', 'class', 'best'];
+// document.querySelector('button').onclick = function () {
+
+//   one.innerHTML += m.length -1 + '___ ';
+// one.innerHTML += m.length + '___ ';
+// };
+
+
+// let one = document.querySelector('.one');
+// let m = [1, 2, 1, 3, 4, 2, 10,  5, 6, 4, 9, 10];
+// function f1() {
+
+// let a = Array.from(new Set(m));
+
+//   one.innerHTML += a;
+// }
+// f1(m);
+
+
+// let one = document.querySelector('.one');
+// let a = [1, 2, 3, 'hello', 66];
+
+// function f1() {
+
+//   for ( let i = a.length - 1 ; i > -1; i--) {
+
+// one.innerHTML += a[i];
+
+//   }
+//   }   
+// f1();
+
+// let one = document.querySelector('.one');
+// let a = [0, 2, 5, -4, 6, 22, -9, -12, 8, 12, 13, 78];
+
+// for ( let i = 0 ; i <= a.length -1; i++) {
+//  if ( a[i] > 0) {
+
+//   one.innerHTML += a[i] + ' ';
+// }
+// }
+
+// let one = document.querySelector('.one');
+// let one2 = document.querySelector('.one2');
+// let ar16 = [0, 2, 5, -4, 6, 22, -9, -12, 8, 12, 13, 78];
+// let odd = [];
+// let even = [];
+
+// for ( let i = 0 ; i <= ar16.length -1 ; i++) {
+// if ( ar16[i] % 2 == 0) {
+//   odd.push(ar16[i]);  
+
+// }else {
+//   even.push(ar16[i]);
+
+// } 
+// }
+// one.innerHTML += odd;
+// one2.innerHTML += even;
+
+
+// let one = document.querySelector('.one');
+// let ar17 =  [3, 0, 2, 6, 0, 1, 3, 1, 9, 0, 2, 0];
+
+// for ( let i = 0 ; i <= ar17.length -1 ; i++) {
+//   if(ar17[i] > 2) {
+//   one.innerHTML += ar17[i] + ' ';
+// }
+// }
+
+// let one = document.querySelector('.one');
+// let ar18 =  [1,15,24,13,78,21,4,45,67,91];
+// let max = Math.max(...ar18);
+
+// one.innerHTML += max;
+
+// let one = document.querySelector('.one');
+// let arr2 = [15,4,5,1];
+// function f1(ar){
+// let sum = 0;
+// for(let i = 0; i < ar.length; i++){
+//     sum += ar[i];
+//     }
+//     one.innerHTML += sum;
+// }
+// f1(arr2);
+// !!!!!!!!
+
+// let one = document.querySelector('.one'); 
+// let out = document.querySelector('.out'); 
+// let d1 = [33,'best', 66,'best'];
+// function f1() {
+
+//  d1.push(out.value);
+
+// one.innerHTML += d1 + ' ';
+//  }
+//  document.querySelector('button').onclick = f1;
+
+
+// let one = document.querySelector('.one'); 
+// let out = document.querySelector('.out'); 
+
+// function f2() {
+// let d1 = [33,'best', 66, 'best',77];
+
+//  out.value = d1.shift();
+//  one.innerHTML += d1;
+
+// }
+// f2();
+
+let one = document.querySelector('.one');
+let input = document.querySelector('.input');
+let button = document.querySelector('button');
+// let m = [1,2,3,4,5];
+
+// function f3 () {
+
+// m.reverse(out.value);  
+//       one.innerHTML += m + ' ';
+
+// }  
+// document.querySelector('button').onclick = f3;
+
+
+//  let d11 = [2,3,4,5,6,7];
+
+
+// document.querySelector('button').onclick = function f3() {
+
+//     one.innerHTML += d11.indexOf(+input.value);
+
+// };
+
+// let d15 = [0, 2, 5, -4, 6, 22, -9, -12, 8, 12, 13, 78];
+
+
+// document.querySelector('button').onclick = function f1() {
+//     let all = true;
+//     for (let i = 0; i < d15.length; i++) {
+
+//         if (+input.value === d15[i]) {
+//             all = false;
+//             one.innerHTML += 'Есть такое число';
+//             break;
+//         }
+
+//     }
+//     if (all) {
+//         d15.push(+input.value);
+//     }
+//     for (let i = 0; i < d15.length; i++) {
+
+//         one.innerHTML += d15[i] + ' ';
+
+//     }
+// };
+
+
+// let d16 = [];
+// let d161 = [5,6,7,8,9];
+// let d162 = [23,24,56,87,77];
+
+// let a = d161.concat(d162);
+// d16.push(a);
+// one.innerHTML += (d16);
+
+// let dOut=[];
+// let d1=['a', 'b', 'c', 'd'];
+// let d2=[1,2,3,4,5];
+// document.querySelector('button').onclick = function f1() {
+//     for (let i = 0; i < d1.length; i++) {
+
+
+// break;
+//     }for (let k = 0; k < d2.length; k++) {
+
+//         break;
+//     }
+//     dOut.push(d1,d2);
+//     one.innerHTML += (dOut);
+// };
+
+
+// let d18 = ['b', 'c', '45', 'e', 'z', 'y'];
+// document.querySelector('button').onclick = () => {
+//     one.innerHTML += (d18.includes(input.value));
+// };
+
+
+// let m = ['Your', 'payment', 'method', 'will', 'automaticall', 'be', 'charged', 'in', 'advance', 'every'];
+// document.querySelector('button').onclick = function () {
+//     let a = 0;
+//     for (let i = 0; i < m.length; i++) {
+//         if (m[i].length > a) {
+//             a = m[i].length;
+
+//             for (let k = a; k > m.length; k++) {
+//                 let k = m[i];
+//                 one.innerHTML += k;
+//                 return;
+//             }
+
+//         }
+//     }
+
+// };
